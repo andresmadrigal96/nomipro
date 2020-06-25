34e3c7d7973d3c36db3c57684819b231d73c7b12
andresmadrigal96 <andresmadrigalpelaez@gmail.com>
2020-06-25 00:53:43 -0500

    Arreglamos la consulta

--- controller/insertControlPago.php
+++ controller/insertControlPago.php
@@ -2,24 +2,21 @@
 
 require 'conexion.php';
 
-$nombre = $_POST['idEmpleado_fk'];
+$empleado_id = $_POST['idEmpleado_fk'];
 
 $consulta = "SELECT SUM(valor*numeroHoras) as the FROM hextraxempleado 
 INNER JOIN horasextra ON hextraxempleado.idHExtra_fk=horasextra.idHExtra 
-INNER JOIN empleados ON hextraxempleado.idEmpleado_fk=empleados.idEmpleado 
-WHERE nombreE= '$nombre'";
-
+WHERE hextraxempleado.idEmpleado_fk= '$empleado_id'";
 
 
 $consulta2 = "SELECT SUM(valor) FROM parafiscalesxempleados 
 INNER JOIN parafiscales ON parafiscalesxempleados.idParafiscal_fk=parafiscales.idParafiscal 
-INNER JOIN empleados ON parafiscalesxempleados.idEmpleado_fk=empleados.idEmpleado 
-WHERE nombreE= '$nombre'";
+WHERE parafiscalesxempleados.idEmpleado_fk= '$empleado_id'";
 
 
 $estado = $_POST['estado'];
 
-$query = "INSERT INTO controlpagos VALUES(NULL, '$nombre','$consulta','$consulta2','$estado')";
+$query = "INSERT INTO controlpagos VALUES(NULL, $empleado_id,($consulta),($consulta2),'$estado')";
 $mysqli ->query($query);
 
 if($mysqli){
